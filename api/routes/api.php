ea848549353a02121142f89541ddeb11f59ae177
Julio Cesar Moreira de Oliveira <joliveira@informs.conder>
2019-12-18 11:16:05 -0300

    Criada rota de município

--- api/routes/api.php
+++ api/routes/api.php
@@ -25,6 +25,7 @@ Route::group(['middleware' => 'token.validation'], function () {
     Route::resource('pais', '\Diarias\Http\Controllers\PaisController');
     Route::resource('classe', '\Diarias\Http\Controllers\ClasseController');
     Route::resource('gratificacao', '\Diarias\Http\Controllers\GratificacaoController');
+    Route::resource('municipio', '\Diarias\Http\Controllers\MunicipioController');
     Route::resource('prerrogativa', '\Diarias\Http\Controllers\PrerrogativaController');
     Route::middleware('criacao.usuario')->post('funcionario', '\Diarias\Http\Controllers\FuncionarioController@store');
     Route::resource('funcionario', '\Diarias\Http\Controllers\FuncionarioController')->only(['index', 'show', 'update']);
